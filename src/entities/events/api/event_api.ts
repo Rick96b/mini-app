a16c9ff91420b2511cab10b081ce308f550f9f54
qiniese <qiniese@gmail.com>
2024-03-07 15:44:52 +0500

    added event entity

---
+++ src/entities/events/api/event_api.ts
@@ -0,0 +1,33 @@
+import { arrayUnion, collection, doc, getDocs, increment, updateDoc } from "firebase/firestore"
+import { db } from "shared/firebase"
+import { Events } from "../model/event_types"
+
+export const addToCommand = async ( event: Events, commandName: string ) => 
+{
+    updateDoc  
+    (
+        doc(db, `commands/${commandName}`), 
+        {
+            events: arrayUnion( event.name ),
+            raiting: increment( event.rating )
+        }
+    )
+    updateDoc
+    (
+        doc( db, `events/${event.name}` ), 
+        {
+        commandName: commandName
+        }
+    )
+}
+
+export const getAllEvents = async () => 
+{
+    let result: Events[] = []
+    const eventCollection = ( await getDocs(collection(db, 'events' ) ) )
+    eventCollection.forEach( event => 
+    {
+        result.push( event.data() as Events )
+    });
+    return result
+}
