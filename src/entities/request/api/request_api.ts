8fb616fa9ae4b0f7fd0432bbb746d01fb980994a
qiniese <qiniese@gmail.com>
2024-03-09 19:21:12 +0500

    request methods

--- src/entities/request/api/request_api.ts
+++ src/entities/request/api/request_api.ts
@@ -1,4 +1,36 @@
 import { arrayUnion, collection, doc, getDocs, increment, updateDoc } from "firebase/firestore"
 import { db } from "shared/firebase"
-import { Request } from "../model/request_types"
+import { BuildingsRequest, ItemsRequest, Request } from "../model/request_types"
+
+// методы для предметов //
+
+export const getAllItemRequests = async () => 
+{
+
+    let result: ItemsRequest[] = []
+    const itemsRequestCollection = ( await getDocs(collection(db, 'items_request' ) ) )
+    itemsRequestCollection.forEach( itemsRequest => 
+    {
+        result.push( itemsRequest.data() as ItemsRequest )
+    });
+    return result
+
+}
+
+// методы для строений //
+
+export const getAllBuildingRequests = async () => 
+{
+
+    let result: BuildingsRequest[] = []
+    const buildingsRequestCollection = ( await getDocs(collection(db, 'buildings_request' ) ) )
+    buildingsRequestCollection.forEach( buildingsRequest => 
+    {
+        result.push( buildingsRequest.data() as BuildingsRequest )
+    });
+    return result
+
+}
+
+// общие методы //
 
--- src/entities/request/model/request_types.ts
+++ src/entities/request/model/request_types.ts
@@ -1,19 +1,22 @@
+import { ExploreOffTwoTone } from "@mui/icons-material"
+
 export type Request =
 {
+    itemsRequest: ItemsRequest
+    buildingsRequest: BuildingsRequest
+}
 
-    itemsRequest: 
-    {
-        items: string[]
-        isApprovedByBank: boolean
-        isCompleted: boolean
-    }
+export type ItemsRequest =
+{
+    items: string[]
+    isApprovedByBank: boolean
+    isCompleted: boolean
+}
 
-    buildingsRequest:
-    {
-        buildings: boolean
-        isApprovedByBank: boolean
-        isApprovedByConstruction: boolean
-        isCompleted: boolean
-    }
-
+export type BuildingsRequest = 
+{
+    buildings: boolean
+    isApprovedByBank: boolean
+    isApprovedByConstruction: boolean
+    isCompleted: boolean
 }
